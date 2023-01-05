fc42a1ff8102a2c94c119e18146ba557d50422ea
DanDevelop22 <dan22.develop@gmail.com>
2023-01-05 16:00:07 -0500

    api Ultimos paints, cross origin

--- authentication/admin.py
+++ authentication/admin.py
@@ -3,4 +3,13 @@ from .models import *
 
 # Register your models here.
 
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email']
+    
+    def save_model(self, request, obj, form, change):
+        if obj.password.startswith('pbkdf2'):
+            obj.password=obj.password
+        else:
+            obj.set_password(obj.password) 
+        super().save_model(request, obj, form, change)
