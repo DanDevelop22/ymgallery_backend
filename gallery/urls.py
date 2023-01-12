8a14d7910ec03bd8c4518e8b0fe9344f0ab43d12
DanDevelop22 <dan22.develop@gmail.com>
2023-01-12 17:32:07 -0500

    Api view paint filter

--- gallery/urls.py
+++ gallery/urls.py
@@ -4,5 +4,5 @@ from .views import PaintLastAPIView, PaintRecommendedAPIView, PaintFilterAPIView
 urlpatterns = [
    path('paint-last/', PaintLastAPIView.as_view()),
    path('paint-recommended/', PaintRecommendedAPIView.as_view()),
-   path('paint-filter', PaintFilterAPIView.as_view())
+   path('paint-filter/', PaintFilterAPIView.as_view())
 ]
